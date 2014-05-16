2a6853a746b0027d3d4b6546c66ef36c10887428
Kirill-alekseevich <kirill_kudo93@mail.ru>
2014-05-16 07:59:22 +0000

    #104 Добавлены иконки для шаблона Критерия Фильтра

--- code/js/classes/FilterOption.class.js
+++ code/js/classes/FilterOption.class.js
@@ -10,6 +10,7 @@ function FilterOption( icon, name ) {
         if ( this.configuration ) {
             filterOptionBlock.appendChild( showConfiguration( this ) ) ;
         } else {
+            filterOptionBlock.appendChild( showIcon( this ) );
             filterOptionBlock.appendChild( showName( this ) );
         }
         
@@ -18,7 +19,7 @@ function FilterOption( icon, name ) {
     
     this.showEditInterface = function() {
         var editInterfaceBlock = document.createElement( 'div' );
-        editInterfaceBlock.textContent = name;
+        editInterfaceBlock.textContent = 'editable ' + name;
         $( editInterfaceBlock ).addClass( 'editInterface' );
         
         return editInterfaceBlock;
@@ -26,13 +27,27 @@ function FilterOption( icon, name ) {
     
     function showConfiguration( filterOption ) {
         var configurationBlock = document.createElement( 'div' );
+        
         configurationBlock.textContent = 'configured';
+        
         return configurationBlock;
+    }
+    
+    function showIcon( filterOption ) {
+        var iconBlock = document.createElement( 'img' );
+        
+        iconBlock.src = filterOption.icon;
+        $( iconBlock ).addClass( 'icon' );
+        
+        return iconBlock;
     }
     
     function showName( filterOption ) {
         var nameBlock = document.createElement( 'div' );
+        
         nameBlock.textContent = filterOption.name;
+        $( nameBlock ).addClass( 'name' );
+        
         return nameBlock;
     }
 }
