920d73b84c025f8db61663500055a099d4b793ea
los-romka <sin_ss@list.ru>
2014-06-06 21:08:38 +1100

    #178 users selection added

--- code/js/classes/User.class.js
+++ code/js/classes/User.class.js
@@ -7,19 +7,30 @@ function User( id, firstName, lastName, photo, age, sex, city, circles ) {
     this.sex = sex;
     this.city = city;
     this.circles = circles;
+    this.checked = false;
     
     this.show = function() {
         var userBlock = document.createElement( 'div' ),
               img = document.createElement( 'img' ),
-              span = document.createElement( 'span' );
+              span = document.createElement( 'span' ),
+              self = this;
               
         $( userBlock ).addClass( 'user' );
+        
+        if ( this.checked ) {
+            $( userBlock ).addClass( 'selected' );
+        }
         
         img.src = this.photo;
         span.textContent = this.firstName + ' ' + this.lastName;
         
         userBlock.appendChild( img );
         userBlock.appendChild( span );
+        
+        userBlock.addEventListener( 'click', function( e ) {
+            $( this ).toggleClass( 'selected' );
+            self.checked = !self.checked;
+        });
         
         return userBlock;
     }
