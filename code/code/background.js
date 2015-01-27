b7621182293fc2205e85c7c4c5d34436df8b05a7
Yuri-Ivanenko <yurown92@yahoo.com>
2015-01-27 15:18:18 +1100

    Edit Filter Followers

--- code/code/background.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-chrome.browserAction.onClicked.addListener( function( activeTab ) {
-    getTokenOAuth2( function( token ) { 
-        chrome.tabs.create( { 'url' : chrome.extension.getURL( 'index.html' ), 'selected' : true } );
-    });
-});
